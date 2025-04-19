a72ba64e78695169c2f13a07f93d6f005ccff71b
Lei Aguila <aguila.christleidaniel@gmail.com>
2025-04-20 06:55:42 +0800

    added contact me and a new section in about me

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -2,6 +2,8 @@ import * as React from "react";
 import "../globals.css";
 import TopNav from "@/components/TopNav";
 import Image from "next/image";
+import { Tooltip } from "@mui/material";
+import Paper from "@mui/material/Paper";
 
 export default function About() {
   return (
@@ -18,31 +20,87 @@ export default function About() {
           quick intro to who I am, what I do, and why I love it. Stick around
           maybe we'll build something great together!
         </p>
-        <a href="#why-ue" className="text-white">
-          WHY UE?
-        </a>
+      </div>
+      <div className="flex flex-col m-2 p-2 justify-center items-center">
+        <h6 className="text-white text-xl tracking-wide">
+          Why I chose University of the East
+        </h6>
+        <Tooltip title="Click to scroll down">
+          <a href="#why-ue" className="text-white">
+            <Image
+              src="/arrow_down.png"
+              alt="arrow down"
+              width={100}
+              height={100}
+            />
+          </a>
+        </Tooltip>
       </div>
       <div
         className="bg-[url('/ue_wallpaper.jpg')] bg-fixed bg-cover bg-end h-96 w-full"
         style={{ backgroundImage: "url('/ue_wallpaper.jpg')" }}
       ></div>
+      <br />
       <div className="m-4 p-4 flex flex-col" id="why-ue">
         <h1 className="text-white text-3xl font-bold tracking-wide">
           Why I Chose UE for My Computer Science Studies
         </h1>
-        <p className="text-gray-200 tracking-normal text-justify m-2 p-2">
+        <p className="text-zinc-500 text-xl tracking-normal text-justify m-2 p-2">
           I decided to pursue my Computer Science degree at the University of
           the East (UE) because of its strong CHED accreditation, ensuring a
           high-quality education that meets national standards. At the time, it
           was the most cost-effective institution that delivered top-notch
           Computer Science education, making it the perfect choice for me as a
-          student seeking both value and quality.Plus, red has always been my
+          student seeking both value and quality. Plus, red has always been my
           favorite color, and UE’s vibrant school colors made me feel even more
           connected to its spirited academic community. The combination of
           academic credibility, affordability, and a lively campus environment
           made UE the ideal place for me to grow as a future IT professional.
         </p>
       </div>
+      <div className="flex flex-col m-2 p-2 justify-center items-center">
+        <h6 className="text-white text-xl tracking-wide">
+          My Free time Activities
+        </h6>
+        <Tooltip title="Click to scroll down">
+          <a href="#hobby" className="text-white">
+            <Image
+              src="/arrow_down.png"
+              alt="arrow down"
+              width={100}
+              height={100}
+            />
+          </a>
+        </Tooltip>
+      </div>
+      <div
+        className="mt-12 bg-[url('/f1_car.jpg')] bg-fixed bg-cover bg-end h-96 w-full"
+        style={{ backgroundImage: "url('/f1_car.jpg')" }}
+      ></div>
+      <div className="m-4 p-4 flex flex-col" id="hobby">
+        <h1 className="text-white text-3xl font-bold tracking-wide">
+          Formula 1 
+        </h1>
+        <p className="text-zinc-500 text-xl tracking-normal text-justify m-2 p-2">
+          In my free time, one of my favorite hobbies is following Formula 1.
+          There’s something incredibly intriguing about the perfect blend of
+          speed, strategy, and cutting-edge technology. It’s not just about fast
+          cars racing around a track it’s a complex sport where every
+          millisecond counts, and tiny decisions can change the outcome of an
+          entire race. What fascinates me most is the constant battle between
+          teams to innovate and gain even the slightest edge, whether it's
+          through aerodynamics, tire strategy, or pit stop timing. The rivalry
+          between drivers, the unpredictability of weather and crashes, and the
+          global scale of the sport from Monaco to Japan make every race feel
+          like an adrenaline-filled chess match. Watching F1 isn't just
+          exciting it’s a deep dive into precision, teamwork, and relentless
+          competition, and that’s what keeps me hooked.
+        </p>
+      </div>
+      <br />
+      <br />
+      <br />
+      <br />
     </div>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,9 @@ import "./globals.css";
 import { DM_Serif_Display } from "next/font/google";
 import { Open_Sans } from "next/font/google";
 import Image from "next/image";
-import { Divider } from "@mui/material";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import FacebookIcon from '@mui/icons-material/Facebook';
 const dm_serif = DM_Serif_Display({
   subsets: ["latin"],
   weight: "400",
@@ -30,20 +32,42 @@ export default function Page() {
           </div>
           {/* my short description */}
           <div className="flex flex-col justify-center items-center mt-8">
-            <p className={`${open_sans.className} text-zinc-500 tracking-normal text-justify`}>
+            <p
+              className={`${open_sans.className} text-2xl text-zinc-500 tracking-normal text-justify`}
+            >
               Motivated Computer Science student skilled in programming and
-              graphic design, with expertise in technology. Strong leadership and
-              multitasker, adept at managing projects and collaborating under
-              pressure. Combines technical skills with creativity to build
+              graphic design, with expertise in technology. Strong leadership
+              and multitasker, adept at managing projects and collaborating
+              under pressure. Combines technical skills with creativity to build
               engaging solutions. Eager to contribute in dynamic environments
               and drive impactful results.
             </p>
+            <div className="text-white mt-2 pt-12">
+              <h1
+                className={`${open_sans.className} pb-4 text-white text-xl font-bold`}
+              >
+                Connect with me:
+              </h1>
+              <div className="flex flex-row gap-4">
+                <a href="https://github.com/leiDanielAguila" target="_blank">
+                  <GitHubIcon style={{ fontSize: 40, color: "white" }} />
+                </a>
+                <a
+                  href="https://www.linkedin.com/in/christlei-daniel-aguila-bb2215318/"
+                  target="_blank"
+                >
+                  <LinkedInIcon style={{ fontSize: 40, color: "white" }} />
+                </a>
+                <a href="https://www.facebook.com/lei.daniel.2025/" target="_blank">
+                  <FacebookIcon style={{ fontSize: 40, color: "white" }} />
+                </a>
+              </div>
+            </div>
           </div>
-          <div>{/* my social media links */}</div>
         </div>
         {/* my picture */}
-        <div className="w-1/2 ">
-          <Image src="/me.png" alt="my picture" width={400} height={400} />
+        <div className="w-1/2">
+          <Image src="/me.png" alt="my picture" width={400} height={400} className="ml-42"/>
         </div>
       </div>
     </div>
